a99a92ef08d7c55e4faab478a93c43616d3220d7
musab700 <musabmeo708@gmail.com>
2020-08-11 18:42:08 +0200

    Started from scratch

--- main.py
+++ main.py
@@ -8,61 +8,17 @@ client = commands.Bot(command_prefix='.')
 
 @client.event
 async def on_ready():
-    print("Bot is connected")
-
-
-@client.event
-async def on_disconnect():
-    print("Bot disconnected")
-
-
-# Writes the messages in logs.txt and displays them in console
+    print('Bot is connected')
 
 
 @client.event
 async def on_message(message):
-    await client.process_commands(message)
-    try:
-        with open('logs.txt', 'a') as file:
-            file.write(f'{message.author}: {message.content} \n')
-    except FileNotFoundError:
-        print("Error loading file")
-
-    if message.author == client:
-        return
-    else:
-        print(f'{message.author}:{message.content}')
-
-
-@client.event
-async def on_member_join(member, channel):
-    await channel.send(f'{member} joined')
-    print(f'{member} has joined')
-
-
-@client.event
-async def on_member_remove(member, channel):
-    await channel.send(f'{member} left')
-    print(f'{member} has joined')
-
-
-# ____ Commands section ____
-
-@client.command()
-async def ping(message):
-    await message.channel.send(f'Pong: {round(client.latency * 1000)} ms')
-
-
-@client.command()
-async def coinf(message):
-    num = random.randint(0, 1)
-    if num == 0:
-        await message.channel.send('Heads')
-    else:
-        await message.channel.send('Tails')
+    with open('logs.txt', 'a') as file:
+        file.write(f'{message.author}:{message.content}')
+    print(f'{message.author}:{message.content}')
 
 
 with open("properties.json") as properties:
     data = json.load(properties)
 
-client.run(data["token"])
+client.run(data.token)
