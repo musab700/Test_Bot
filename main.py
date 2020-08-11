65583f670e131fcbf35f805ea5f23b4c9efea9c1
musab700 <musabmeo708@gmail.com>
2020-08-11 19:33:03 +0200

    added ping command

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix='.')
+client = commands.Bot('.')
 
 
 @client.event
@@ -11,14 +11,26 @@ async def on_ready():
     print('Bot is connected')
 
 
+# logs messages into logs.txt
 @client.event
 async def on_message(message):
-    with open('logs.txt', 'a') as file:
-        file.write(f'{message.author}:{message.content}')
-    print(f'{message.author}:{message.content}')
+
+    if message.author == client.user:
+        return
+    else:
+        with open('logs.txt', 'a') as file:
+            file.write(f'{message.author}:{message.content}\n')
+            print(f'{message.author}:{message.content}')
+        await client.process_commands(message)
+
+
+# checks client ping
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Ping: {round(client.latency*1000)}ms')
 
 
 with open("properties.json") as properties:
     data = json.load(properties)
 
-client.run(data.token)
+client.run(data['token'])
