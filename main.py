bba23c05482661ef2321103053a3f79b49c7e5c3
musab700 <musabmeo708@gmail.com>
2020-08-14 21:32:48 +0200

    removed coinflip due to it not working correctly

--- main.py
+++ main.py
@@ -22,22 +22,6 @@ async def ping(ctx):
 
 
 @client.command()
-async def cf(message, arg):
-    is_heads = random.randint(0, 1)
-    input_heads = arg == 'h'
-    if is_heads == input_heads:
-        embed_win = discord.Embed(title="",
-                                  description="**You win!** It was {}".format('heads' if is_heads else 'tails'),
-                                  color=0xff3d00)
-        await message.channel.send(embed=embed_win)
-    else:
-        embed_lose = discord.Embed(title="",
-                                   description="**You lose!**, It was {}".format('heads' if is_heads else 'tails'),
-                                   color=0xff3d00)
-        await message.channel.send(embed=embed_lose)
-
-
-@client.command()
 async def register(ctx):
     user = str(ctx.message.author.id)
     if user not in data:
