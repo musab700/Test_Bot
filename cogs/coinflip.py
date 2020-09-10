d93b6691dcde5ad50bb001a2c548286c23c3ab6c
musab700 <musabmeo708@gmail.com>
2020-09-10 08:17:28 +0200

    starting from scratch

---
+++ cogs/coinflip.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import random
+
+
+class Coinflip(commands.Cog()):
+    def __int__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def coinflip(self, ctx, arg1):
+        outcome = random.randint(0, 1)
+        if arg1 == "h" or "t":
+            if (arg1 == "h" and outcome == 0) or (arg1 == "t" and outcome == 1):
+                await ctx.send("You win")
+            else:
+                await ctx.send("You lose")
+        else:
+            await ctx.send("Invalid usage")
+
+
+def setup(bot):
+    bot.add_cog(Coinflip(bot))
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import discord
 from discord.ext import commands
+import json
 
 
 client = commands.Bot(command_prefix="?")
+client.load_extension("cogs.coinflip")
 
+
+with open("properties.json") as properties:
+    prop = json.load(properties)
+
+TOKEN = prop['token']
+
+client.run(TOKEN)
